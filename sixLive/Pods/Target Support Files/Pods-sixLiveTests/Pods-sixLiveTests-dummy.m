ea42c96ebbefe1cee6d53f75976b085df04866a2
吴贞利 <1363721033@qq.com>
2017-06-27 16:30:47 +0800

    1、添加AFN、SDWebImage

---
+++ sixLive/Pods/Target Support Files/Pods-sixLiveTests/Pods-sixLiveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sixLiveTests : NSObject
+@end
+@implementation PodsDummy_Pods_sixLiveTests
+@end
---
+++ sixLive/Pods/Target Support Files/Pods-sixLiveUITests/Pods-sixLiveUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sixLiveUITests : NSObject
+@end
+@implementation PodsDummy_Pods_sixLiveUITests
+@end
